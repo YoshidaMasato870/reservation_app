f6abfec1584409289e6374a4cb49929fa8b452fb
吉田優斗 <masato@yoshidayuutonoMacBook-Air.local>
2024-08-28 14:27:14 +0900

    施設登録後に予約ページへ遷移するよう変更しました

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,9 +9,11 @@ class RoomsController < ApplicationController
 
     def create
         @room = Room.new(params.require(:room).permit(:name, :info, :price, :address, :image, :user_id))
-        if @room.save 
-            redirect_to rooms_path
+        if @room.save
+            flash[:notice] = "施設の登録に成功しました" 
+            redirect_to reservation_path(@room.id)
         else
+            flash[:alert] = "施設の登録に失敗しました" 
             render new_room_path
         end
     end
@@ -24,8 +26,10 @@ class RoomsController < ApplicationController
         @room = Room.find(params[:id])
         if @room.update(params.require(:room)
             .permit(:name, :info, :price, :address, :image))
+            flash[:notice] = "施設の情報を更新しました" 
             redirect_to rooms_path
         else
+            flash.now[:alert] = "施設の情報の更新に失敗しました" 
             render "edit"
         end
     end
@@ -33,6 +37,7 @@ class RoomsController < ApplicationController
     def destroy
         @room = Room.find(params[:id])
         @room.destroy
+        flash[:notice] = "施設の情報を削除しました" 
         redirect_to rooms_path
     end
 end
